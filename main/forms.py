3f9b2529fa5da9bc8880304bc644e406968abb86
Andrew Skow <anjeie24@mail.ru>
2025-11-23 16:34:37 +0300

    Validation and Stylization Form

--- main/forms.py
+++ main/forms.py
@@ -1,14 +1,29 @@
 from django.forms import ModelForm
 from .models import Student, Subject
+from django import forms
 
 
-class StudentForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            # widget - менеджер поля, который отвечает за отображение
+            field.widget.attrs["class"] = "form-control"
+
+
+class StudentForm(StyleFormMixin, ModelForm):
     class Meta:
         fields = "__all__"
         model = Student
 
+    def clean_email(self):
+        cleaned_data = self.cleaned_data["email"]
+        if "mail.ru" not in cleaned_data:
+            raise forms.ValidationError("Почта не относится к домену mail.ru")
+        return cleaned_data
 
-class SubjectForm(ModelForm):
+
+class SubjectForm(StudentForm, ModelForm):
     class Meta:
         fields = "__all__"
         model = Subject
--- main/models.py
+++ main/models.py
@@ -10,6 +10,9 @@ class Student(models.Model):
     first_name = models.CharField(max_length=255, verbose_name="Имя")
     last_name = models.CharField(max_length=255, verbose_name="Фамилия")
     avatar = models.ImageField(upload_to="students/", verbose_name="аватар", **NULLABLE)
+    email = models.CharField(
+        max_length=255, verbose_name="Почта", unique=True, **NULLABLE
+    )
     is_active = models.BooleanField(default=True, verbose_name="Активный")
 
     def __str__(self):
